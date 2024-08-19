01eb4d80ffc8c75e594f7d4d31ab6a11da004a9c
Sphesihl3 <sphesihle.mabaso@xetgroup.com>
2024-08-19 20:51:07 +0200

    fixed UI

--- EventSystem/Models/Event.cs
+++ EventSystem/Models/Event.cs
@@ -1,17 +1,28 @@
-﻿namespace EventSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventSystem.Models
 {
     public class Event
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Total seats must be at least 1.")]
         public int TotalSeats { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Seats available cannot be negative.")]
         public int SeatsAvailable { get; set; }
+
         public ICollection<Registration> Registrations { get; set; }
+
+        [StringLength(1000)]
         public string Description { get; set; }
-
-
     }
+
 
 }
--- EventSystem/Models/Registration.cs
+++ EventSystem/Models/Registration.cs
@@ -6,15 +6,24 @@ namespace EventSystem.Models
     public class Registration
     {
         public int Id { get; set; }
+
+        [Required]
         public int EventId { get; set; }
+
         public Event Event { get; set; }
+
+        [Required]
         public string UserId { get; set; }
-        public IdentityUser User { get; set; } 
+
+        public IdentityUser User { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "Reference number cannot exceed 50 characters.")]
         public string ReferenceNumber { get; set; }
 
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         public string EmailAddress { get; set; }
-       
     }
+
 }
